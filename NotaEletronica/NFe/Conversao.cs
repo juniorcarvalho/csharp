beea45e31c0b85daa2b92cc85ab564499afac17f
Júnior Carvalho <joseadolfojr@gmail.com>
2015-04-28 22:54:04 -0300

    Novas Classes

--- NotaEletronica/NFe/Conversao.cs
+++ NotaEletronica/NFe/Conversao.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NFe
+﻿namespace NFe
 {
     public static class Conversao
     {
@@ -12,14 +6,49 @@ namespace NFe
         public enum TipoNF { entrada, saida };
         public enum DestinoOperacao { doInterna = 1, doInterestadual = 2, doExterior = 3 };
         public enum ECFModRef { ECFModRefVazio, ECFModRef2B, ECFModRef2C, ECFModRef2D };
-        public enum TipoImpressao { tiSemGeracao, tiRetrato, tiPaisagem, tiSimplificado, tiNFCe, tiMsgEletronica, tiNFCeA4 };
-        public enum TipoEmissao { teNormal = 1, teContingencia = 2, teSCAN = 3, teDPEC = 4, teFSDA = 5, teSVCAN = 6, teSVCRS = 7, teOffLine = 9 };
+        public enum TipoImpressao
+        {
+            tiSemGeracao, tiRetrato, tiPaisagem, tiSimplificado, tiNFCe, tiMsgEletronica, tiNFCeA4
+        };
+        public enum TipoEmissao
+        {
+            teNormal = 1, teContingencia = 2, teSCAN = 3, teDPEC = 4, teFSDA = 5, teSVCAN = 6,
+            teSVCRS = 7, teOffLine = 9
+        };
         public enum TipoAmbiente { taProducao = 1, taHomologacao = 2 };
         public enum FinalidadeNFe { fnNormal = 1, fnComplementar = 2, fnAjuste = 3, fnDevolucao = 4 };
-        public enum ProcessoEmissao { peAplicativoContribuinte, peAvulsaFisco, peAvulsaContribuinte, peContribuinteAplicativoFisco };
+        public enum ProcessoEmissao
+        {
+            peAplicativoContribuinte, peAvulsaFisco, peAvulsaContribuinte, peContribuinteAplicativoFisco
+        };
         public enum ConsumidorFinal { cfNao, cfConsumidorFinal };
-        public enum PresencaComprador { pcNao, pcPresencial, pcInternet, pcTeleatendimento, pcEntregaDomicilio, pcOutros = 9 }
-        public enum CRT { crtSimplesNacional = 1, crtSimplesExcessoReceita = 2, crtRegimeNormal = 3 }
+        public enum PresencaComprador
+        {
+            pcNao, pcPresencial, pcInternet, pcTeleatendimento, pcEntregaDomicilio, pcOutros = 9
+        };
+        public enum CRT { crtSimplesNacional = 1, crtSimplesExcessoReceita = 2, crtRegimeNormal = 3 };
         public enum indIEDest { inContribuinte = 1, inIsento = 2, inNaoContribuinte = 9 };
+        public enum IndicadorTotal { itSomaTotalNFe, itNaoSomaTotalNFe };
+        public enum TipoViaTransp
+        {
+            tvMaritima = 1, tvFluvial = 2, tvLacustre = 3, tvAerea = 4, tvPostal = 5,
+            tvFerroviaria = 6, tvRodoviaria = 7, tvConduto = 8, tvMeiosProprios = 9,
+            tvEntradaSaidaFicta = 10, tvCourier = 11, tvHandcarry = 12
+        };
+        public enum TipoIntermedio { tiContaPropria = 1, tiContaOrdem = 2, tiEncomenda = 3 };
+        public enum TipoOperacao
+        {
+            toVendaConcessionaria = 1, toFaturamentoDireto = 2, toVendaDireta = 3, toOutros = 0
+        };
+        public enum EspecieVeiculo
+        {
+            evPassageiro = 1, evCarga = 2, evMisto = 3, evCorrida = 4, evTracao = 5, evEspecial = 6
+        };
+        public enum CondicaoVeiculo { cvAcabado = 1, cvInacabado = 2, cvSemiAcabado = 3 };
+        public enum TipoRestricaoVeiculo
+        {
+            trvNenhum, trvAlienacaoF, trvAlienacaoM, trvReservaD, trvPenhor, trvOutras = 9
+        };
+        public enum TipoArma { taUsoPermitido, taUsoRestrito };
     }
 }
--- NotaEletronica/NFe/NotaFiscal.cs
+++ NotaEletronica/NFe/NotaFiscal.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NFe
 {
@@ -13,15 +10,15 @@ namespace NFe
 
     class InfNFe
     {
-        public String ID { get; set; }
+        public string ID { get; set; }
         public Double Versao { get; set; }
     }
 
     class RefNF
     {
         public int CUF { get; set; }
-        public String AAMM { get; set; }
-        public String CNPJ { get; set; }
+        public string AAMM { get; set; }
+        public string CNPJ { get; set; }
         public int Modelo { get; set; }
         public int Serie { get; set; }
         public int NNF { get; set; }
@@ -30,9 +27,9 @@ namespace NFe
     class RefNFP
     {
         public int CUF { get; set; }
-        public String AAMM { get; set; }
-        public String CNPJ { get; set; }
-        public String IE { get; set; }
+        public string AAMM { get; set; }
+        public string CNPJ { get; set; }
+        public string IE { get; set; }
         public int Modelo { get; set; }
         public int Serie { get; set; }
         public int NNF { get; set; }
@@ -41,14 +38,14 @@ namespace NFe
     class RefECF
     {
         public Conversao.ECFModRef Modelo { get; set; }
-        public String NECF { get; set; }
-        public String NCOO { get; set; }
+        public string NECF { get; set; }
+        public string NCOO { get; set; }
     }
 
     class refCollectionItem
     {
-        public String RefNFe { get; set; }
-        public String RefCTe { get; set; }
+        public string RefNFe { get; set; }
+        public string RefCTe { get; set; }
         public RefNF RefNF { get; set; }
         public RefNFP RefNFP { get; set; }
         public RefECF RefECF { get; set; }
@@ -58,10 +55,10 @@ namespace NFe
     {
         public int CUF { get; set; }
         public int CNF { get; set; }
-        public String NatOp { get; set; }
+        public string NatOp { get; set; }
         public Conversao.IndicadorDePagamento IndicadorDePagamento { get; set; }
-        public String Mod { get; set; }
-        public String Serie { get; set; }
+        public string Mod { get; set; }
+        public string Serie { get; set; }
         public int Nnf { get; set; }
         public DateTime DHEmi { get; set; }
         public DateTime DHSaiEnt { get; set; }
@@ -78,104 +75,257 @@ namespace NFe
         public Conversao.ConsumidorFinal indFinal { get; set; }
         public Conversao.PresencaComprador indPres { get; set; }
         public Conversao.ProcessoEmissao ProcEmi { get; set; }
-        public String VerProc { get; set; }
+        public string VerProc { get; set; }
         public DateTime DhCont { get; set; }
-        public String XJust { get; set; }
+        public string XJust { get; set; }
     }
 
     class EnderEmit
     {
-        public String XLgr { get; set; }
-        public String Nro { get; set; }
-        public String XCpl { get; set; }
-        public String XBairro { get; set; }
+        public string XLgr { get; set; }
+        public string Nro { get; set; }
+        public string XCpl { get; set; }
+        public string XBairro { get; set; }
         public int CMun { get; set; }
-        public String XMun { get; set; }
-        public String UF { get; set; }
+        public string XMun { get; set; }
+        public string UF { get; set; }
         public int CEP { get; set; }
         public int CPais { get; set; }
-        public String XPais { get; set; }
-        public String Fone { get; set; }
+        public string XPais { get; set; }
+        public string Fone { get; set; }
     }
 
     class Emit
     {
-        public String CNPJCPF { get; set; }
-        public String XNome { get; set; }
-        public String XFant { get; set; }
+        public string CNPJCPF { get; set; }
+        public string XNome { get; set; }
+        public string XFant { get; set; }
         public EnderEmit EnderEmit { get; set; }
-        public String IE { get; set; }
-        public String IEST { get; set; }
-        public String IM { get; set; }
-        public String CNAE { get; set; }
+        public string IE { get; set; }
+        public string IEST { get; set; }
+        public string IM { get; set; }
+        public string CNAE { get; set; }
         public Conversao.CRT CRT { get; set; }
     }
 
     class Avulsa
     {
-        public String CNPJ { get; set; }
-        public String XOrgao { get; set; }
-        public String Matr { get; set; }
-        public String XAgente { get; set; }
-        public String Fone { get; set; }
-        public String UF { get; set; }
-        public String NDAR { get; set; }
+        public string CNPJ { get; set; }
+        public string XOrgao { get; set; }
+        public string Matr { get; set; }
+        public string XAgente { get; set; }
+        public string Fone { get; set; }
+        public string UF { get; set; }
+        public string NDAR { get; set; }
         public DateTime DEmi { get; set; }
-        public Double VDAR { get; set; }
-        public String RepEmi { get; set; }
+        public double VDAR { get; set; }
+        public string RepEmi { get; set; }
         public DateTime DPag { get; set; }
     }
 
     class EnderDest
     {
-        public String XLgr { get; set; }
-        public String Nro { get; set; }
-        public String XCpl { get; set; }
-        public String XBairro { get; set; }
+        public string XLgr { get; set; }
+        public string Nro { get; set; }
+        public string XCpl { get; set; }
+        public string XBairro { get; set; }
         public int CMun { get; set; }
-        public String XMun { get; set; }
-        public String UF { get; set; }
+        public string XMun { get; set; }
+        public string UF { get; set; }
         public int CEP { get; set; }
         public int CPais { get; set; }
-        public String XPais { get; set; }
-        public String Fone { get; set; }
+        public string XPais { get; set; }
+        public string Fone { get; set; }
     }
 
     class Dest
     {
-        public String CNPJCPF { get; set; }
-        public String IDEstrangeiro { get; set; }
-        public String XNome { get; set; }
+        public string CNPJCPF { get; set; }
+        public string IDEstrangeiro { get; set; }
+        public string XNome { get; set; }
         public EnderEmit EnderEmit { get; set; }
         public Conversao.indIEDest IndIEDest { get; set; }
-        public String IE { get; set; }
-        public String ISUF { get; set; }
-        public String IM { get; set; }
-        public String Email { get; set; }
+        public string IE { get; set; }
+        public string ISUF { get; set; }
+        public string IM { get; set; }
+        public string Email { get; set; }
     }
 
     class Retirada
     {
-        public String CNPJCPF { get; set; }
-        public String XLgr { get; set; }
-        public String Nro { get; set; }
-        public String XCpl { get; set; }
-        public String XBairro { get; set; }
+        public string CNPJCPF { get; set; }
+        public string XLgr { get; set; }
+        public string Nro { get; set; }
+        public string XCpl { get; set; }
+        public string XBairro { get; set; }
         public int CMun { get; set; }
-        public String XMun { get; set; }
-        public String UF { get; set; }
+        public string XMun { get; set; }
+        public string UF { get; set; }
     }
 
     class Entrega
     {
-        public String CNPJCPF { get; set; }
-        public String XLgr { get; set; }
-        public String Nro { get; set; }
-        public String XCpl { get; set; }
-        public String XBairro { get; set; }
+        public string CNPJCPF { get; set; }
+        public string XLgr { get; set; }
+        public string Nro { get; set; }
+        public string XCpl { get; set; }
+        public string XBairro { get; set; }
         public int CMun { get; set; }
-        public String XMun { get; set; }
-        public String UF { get; set; }
+        public string XMun { get; set; }
+        public string UF { get; set; }
     }
 
+    class Adi
+    {
+        public int NAdicao { get; set; }
+        public int NSeqAdicC { get; set; }
+        public string CFabricante { get; set; }
+        public double VDescDI { get; set; }
+        public string NDraw { get; set; }
+    }
+
+    class DI
+    {
+        public string NDI { get; set; }
+        public DateTime DDI { get; set; }
+        public string XLocDesemb { get; set; }
+        public string UFDesemb { get; set; }
+        public DateTime DDesemb { get; set; }
+        public Conversao.TipoViaTransp TpViaTransp { get; set; }
+        public double VAFRMM { get; set; }
+        public Conversao.TipoIntermedio TpIntermedio { get; set; }
+        public string CNPJ { get; set; }
+        public string UFTerceiro { get; set; }
+        public List<Adi> Adi { get; set; }
+
+    }
+
+    class DetExport
+    {
+        public string nDraw { get; set; }
+        public string NRE { get; set; }
+        public string CHNFe { get; set; }
+        public double QExport { get; set; }
+    }
+
+    class VeicProd
+    {
+        public Conversao.TipoOperacao TpOp { get; set; }
+        public string Chassi { get; set; }
+        public string CCor { get; set; }
+        public string XCor { get; set; }
+        public string Pot { get; set; }
+        public string Cilin { get; set; }
+        public string PesoL { get; set; }
+        public string PesoB { get; set; }
+        public string NSerie { get; set; }
+        public string TpComb { get; set; }
+        public string NMotor { get; set; }
+        public string CMT { get; set; }
+        public string Dist { get; set; }
+        public int AnoMod { get; set; }
+        public int AnoFab { get; set; }
+        public string TpPint { get; set; }
+        public int TpVeic { get; set; }
+        public Conversao.EspecieVeiculo EspVeic { get; set; }
+        public string VIN { get; set; }
+        public Conversao.CondicaoVeiculo CondVeic { get; set; }
+        public string CMod { get; set; }
+        public string CCorDENATRAN { get; set; }
+        public int Lota { get; set; }
+        public Conversao.TipoRestricaoVeiculo TpRest { get; set; }
+    }
+
+    class Med
+    {
+        public string NLote { get; set; }
+        public double QLote { get; set; }
+        public DateTime DFab { get; set; }
+        public DateTime DVal { get; set; }
+        public double VPMC { get; set; }
+    }
+
+    class Arma
+    {
+        public Conversao.TipoArma TpArma { get; set; }
+        public string NSerie { get; set; }
+        public string NCano { get; set; }
+        public string Descr { get; set; }
+
+    }
+
+    class CIDE
+    {
+        public double QBcProd { get; set; }
+        public double VAliqProd { get; set; }
+        public double VCIDE { get; set; }
+    }
+
+    class ICMSComb
+    {
+        public double VBCICMS { get; set; }
+        public double VICMS { get; set; }
+        public double VBCICMSST { get; set; }
+        public double VICMSST { get; set; }
+    }
+
+    class ICMSInter
+    {
+        public double VBCICMSSTDest { get; set; }
+        public double VICMSSTDest { get; set; }
+    }
+
+    class ICMSCons
+    {
+        public double vBCICMSSTCons { get; set; }
+        public double vICMSSTCons { get; set; }
+        public string UFcons { get; set; }
+    }
+
+    class Comb
+    {
+        public int CProdANP { get; set; }
+        public double PMixGN { get; set; }
+        public string CODIF { get; set; }
+        public double QTemp { get; set; }
+        public string UFcons { get; set; }
+        public CIDE CIDE { get; set; }
+        public ICMSComb ICMS { get; set; }
+        public ICMSInter ICMSInter { get; set; }
+        public ICMSCons ICMSCons { get; set; }
+    }
+
+    class Prod
+    {
+        public string CProd { get; set; }
+        public int NItem { get; set; }
+        public string CEAN { get; set; }
+        public string XProd { get; set; }
+        public string NCM { get; set; }
+        public string EXTIPI { get; set; }
+        public string CFOP { get; set; }
+        public string UCom { get; set; }
+        public double QCom { get; set; }
+        public double VUnCom { get; set; }
+        public double VProd { get; set; }
+        public string CEANTrib { get; set; }
+        public string UTrib { get; set; }
+        public double QTrib { get; set; }
+        public double VUnTrib { get; set; }
+        public double VFrete { get; set; }
+        public double VSeg { get; set; }
+        public double VDesc { get; set; }
+        public double VOutro { get; set; }
+        public Conversao.IndicadorTotal IndTot { get; set; }
+        public List<DI> DI { get; set; }
+        public string XPed { get; set; }
+        public int NItemPed { get; set; }
+        public List<DetExport> DetExport { get; set; }
+        public VeicProd VeicProd { get; set; }
+        public List<Med> Med { get; set; }
+        public List<Arma> Arma { get; set; }
+        public Comb Comb { get; set; }
+        public string NRECOPI { get; set; }
+        public string NFCI { get; set; }
+    }
 }
